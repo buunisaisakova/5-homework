54e83586b627f92b7988e823293f38193fe6419c
buunisaisakova <isannisan97@gmail.com>
2022-08-04 15:05:51 +0600

    firstt

---
+++ app.js
@@ -0,0 +1,24 @@
+// 1
+// let getSum;
+// let number5 = prompt("Fift?");
+// let number7 = prompt("Seventh?")
+
+// if(number5<number7) {
+//   getSum = function(){
+//    let sum = 5+7;
+//    console.log(sum)
+//  }
+// }else {
+// console.log("Просим, вести большое число для первого значение!")
+// }
+
+//2
+
+function prinText(){
+console.log("Привет!");
+}
+
+function text(){
+console.log("Как дела?");
+}
+text();
